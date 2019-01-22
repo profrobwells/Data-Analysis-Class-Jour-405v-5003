9f51c1713c5f3bd6f9faeaf97fc24ebf7d20d272
profrobwells <rswells@uark.edu>
2019-01-22 12:57:42 -0600

    New Census and Student Loan lessons
    New Census and Student Loan lessons

--- Exercises/Downloading Data 12-24-18.R
+++ Exercises/Downloading Data 12-24-18.R
@@ -1,6 +1,5 @@
-#Title: Chapter 4 - Machlis - Downloading data
-
-#Updated Jan 22 2019 with Census Exercise
+#Title: Downloading data
+#Jan 22 2019 with Census Exercise
 
 #rio
 install.packages("rio") 
@@ -8,19 +7,139 @@ install.packages("rio")
 #rio handles more than two dozen formats including tab-separated data (with the extension .tsv), 
 #JSON, Stata, and fixed-width format data (.fwf).
 
-#get data for tutorial
-download.file("http://bit.ly/BostonSnowfallCSV", "BostonWinterSnowfalls.csv")
+StudentLoans <- rio::import("AR2016_SMALL.csv")
+glimpse(StudentLoans)
 
-#load into memory
-snowdata <- rio::import("BostonWinterSnowfalls.csv")
+# Number columns
+ncol(StudentLoans)
 
-#-----------------------------#
-# Outline of Loading Data
-#-----------------------------#   
+
+#Alternate download option: Use read.table() works for importing data:      
+
+<img src="Images/ImportingDataTip.jpg", width="300" height="100" />
+  
+
+#--------------------------------------------------------------------#
+#Converting character strings into numeric
+#--------------------------------------------------------------------#
+  
+#What is the character type?
+  
+#Install tibble for the glimpse function
+library (tibble)
+
+glimpse(StudentLoans)  
+  
+#Convert numbers to "numeric" data
+#We want to turn all columns after HMC2 into numeric
+#HMC2 is Column #9
+StudentLoans[9:102] <- lapply(StudentLoans[9:102], as.numeric)
+View(StudentLoans)
+glimpse(StudentLoans)  
+summary(StudentLoans)
+
+#Do some math - average number of white students
+mean(StudentLoans$UGDS_WHITE)
+
+#Why the NA result? NA= Missing Value
+
+# list rows of data that have missing values 
+#mydata[!complete.cases(mydata),]
+
+StudentLoans[!complete.cases(StudentLoans)]
+
+#Doing math on columns with missing values
+
+sum(StudentLoans$UGDS_WHITE, na.rm=TRUE)
+mean(StudentLoans$TUITIONFEE_IN, na.rm=TRUE)
+
+
+
+#--------------------------------------------------------------------#
+# Loading Data from Scratch
+#--------------------------------------------------------------------#
 
 Loading data
 RSQlite - read data from a database
 xlsx - read in Excel spreadsheets
+
+#Import Income data from US Census
+#INCOME IN THE PAST 12 MONTHS (IN 2017 INFLATION-ADJUSTED DOLLARS) 
+#2013-2017 American Community Survey 5-Year Estimates. S1901. All Arkansas Counties
+
+https://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?pid=ACS_17_5YR_S1901&prodType=table
+
+#Load Data
+ArkCo_Income_2017 <- rio::import("Data/ArkCo_Income_2017.csv")
+
+#Look at the table
+View(ArkCo_Income_2017)
+
+# How many rows?  
+nrow(ArkCo_Income_2017)
+
+# How many columns?
+ncol(ArkCo_Income_2017)
+
+#Install tibble for the glimpse function
+library (tibble)
+    
+#Check data types
+glimpse(ArkCo_Income_2017)
+
+#What is the issue? (Don't read ahead and spoil the fun)
+
+#You are reading ahead
+#Caught you.
+
+
+
+#What is the issue? 
+
+
+
+
+#Delete First Row Headers
+#read.csv(.... , skip=1)
+
+ArkCo_Income2_2017_copy <- rio::import("Data/ArkCo_Income2_2017_copy.csv", skip=1)
+
+#Clean Headers - Janitor package
+library(janitor)
+
+# Clean up column names to they are R friendly
+ArkCo_Income_2017 <- janitor::clean_names(ArkCo_Income_2017)
+View(ArkCo_Income_2017)
+     
+
+# Still need to fix column names
+colnames(ArkCo_Income_2017)
+
+#Use setnames from the data.tablepackage will work on data.frames or data.tables
+#Example
+#library(data.table)
+#setnames(d, old = c('a','d'), new = c('anew','dnew'))
+#d
+
+
+#New Names
+library(data.table)
+setnames(ArkCo_Income_2017, old = c('id', 'id2', 'geography', 'households_estimate_total', 
+  'households_estimate_less_than_10_000', 'households_estimate_10_000_to_14_999', 
+  'households_estimate_15_000_to_24_999', 'households_estimate_25_000_to_34_999', 
+  'households_estimate_35_000_to_49_999', 'households_estimate_50_000_to_74_999', 
+  'households_estimate_75_000_to_99_999', 'households_estimate_100_000_to_149_999', 
+  'households_estimate_150_000_to_199_999', 'households_estimate_200_000_or_more',
+  'households_estimate_median_income_dollars', 'households_estimate_mean_income_dollars',
+  'households_estimate_percent_allocated_household_income_in_the_past_12_months',
+  'households_estimate_percent_allocated_family_income_in_the_past_12_months',
+  'households_estimate_percent_allocated_nonfamily_income_in_the_past_12_months'),
+   new = c('id','id2','geography','households_estimate_total','less10_000','10k_to_14_999','15k_to_24_999',
+           '25k_to_34_999', '35k_to_49_999','50k_to_74_999','75k_to_99_999','100k_to_149_999',
+           '150k_to_199_999','200k_plus','median_income','mean_income',
+           'pct_allocated_household_income','pct_allocated_family_income','pct_allocated_nonfamily_income'))
+
+View(ArkCo_Income_2017)  
 
 Manipulating data
 dplyr - fast data work
@@ -32,70 +151,26 @@ rename Rename column(s).
 bind_rows Merge two data frames into one, combining data from columns with the same name.
 
 
-#-----------------------------#
-#Converting character strings into numeric
-#-----------------------------#
-#Import Income data from US Census
-#INCOME IN THE PAST 12 MONTHS (IN 2017 INFLATION-ADJUSTED DOLLARS) 
-#2013-2017 American Community Survey 5-Year Estimates. S1901. All Arkansas Counties
-
-https://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?pid=ACS_17_5YR_S1901&prodType=table
-
-#rio
-install.packages("rio") 
-
-#Load Data
-ArkCo_Income_2017 <- rio::import("Data/ArkCo_Income_2017.csv", skip=1)
-
-#Look at the table
-View(ArkCo_Income_2017)
-
-# How many rows?  
-nrow(ArkCo_Income_2017)
-
-# How many columns?
-ncol(ArkCo_Income_2017)
-
-library (tibble)
-    
-#Check data types
-glimpse(ArkCo_Income_2017)
-
-#What is the issue?
-
-#Delete First Row Headers
-#read.csv(.... , skip=1)
-ArkCo_Income_2017 <- rio::import("Data/ArkCo_Income_2017.csv", skip=1)
-View(ArkCo_Income_2017)
- 
-glimpse(ArkCo_Income_2017)
- 
-  #Remove rows with NA
-  newdata <- ArkCensus[-c(1,3,5), ] 
-View(newdata)
-
-#Convert numbers to "numeric" data
-ArkCensus[2:4] <- lapply(ArkCensus[2:4], as.numeric)
-View(ArkCensus)
-
+#Other data cleaning tricks
 #Change column to number format (first you have to strip out the $)  
 --The $ is a special character  
 -- earnings$TOTAL.EARNINGS <- gsub("\\$", "", earnings$TOTAL.EARNINGS) 
 
---Function to change the format to numeric  
--- earnings$TOTAL.EARNINGS <- as.numeric(earnings$TOTAL.EARNINGS) 
-
---How read.table() works for importing data:      
-  <img src="Images/ImportingDataTip.jpg", width="300" height="100" />
-  
-  --Export data 
+--Export data 
 Write Export output this file to a CSV or Excel  write.csv or write.excel
 write.csv(AR2016_SMALL,"AR2016_SMALL.csv") 
 
 
-#-----------------------------#
-#More Advanced Section
-#-----------------------------#
+#--------------------------------------------------------------------#
+#More Advanced Section from Machlis Book, Ch. 4
+#--------------------------------------------------------------------#
+
+
+#get data for tutorial
+download.file("http://bit.ly/BostonSnowfallCSV", "BostonWinterSnowfalls.csv")
+
+#load into memory
+snowdata <- rio::import("BostonWinterSnowfalls.csv")
 
 
 
