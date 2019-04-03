be5318082324e8bac8f050b0b02a62ff4c193bd5
Rob Wells <rswells@uark.edu>
2019-04-03 07:55:29 -0500

    loading files for wednesday

--- Exercises/Sentiment Analysis Tutorial 3-31-19.R
+++ Exercises/Sentiment Analysis Tutorial 3-31-19.R
@@ -53,7 +53,34 @@ tidy_books <- austen_books() %>%
                                                  ignore_case = TRUE)))) %>%
   ungroup() %>%
   unnest_tokens(word, text)
+
+#Sentiment dictionaries
+nrc_joy <- get_sentiments("nrc") %>% 
+  filter(sentiment == "joy")
+
+tidy_books %>%
+  filter(book == "Emma") %>%
+  inner_join(nrc_joy) %>%
+  count(word, sort = TRUE)
+
+#Measure sentiment
+library(tidyr)
+
+jane_austen_sentiment <- tidy_books %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(book, index = linenumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+#Plot
+library(ggplot2)
+
+ggplot(jane_austen_sentiment, aes(index, sentiment, fill = book)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~book, ncol = 2, scales = "free_x")
+
 #
+
 data(stop_words)
 
 tidy_books <- tidy_books %>%
