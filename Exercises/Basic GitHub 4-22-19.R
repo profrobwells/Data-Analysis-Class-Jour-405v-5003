de455963bb0f8fbb2ba239c54f4f983241a96af6
Rob Wells <rswells@uark.edu>
2019-04-23 16:40:29 -0500

    Prep for Wednesday April 24 class

---
+++ Exercises/Basic GitHub 4-22-19.R
@@ -0,0 +1,54 @@
+#Notes on GitHub
+#
+#Max Harlow Presentation on How to Use GitHub
+https://docs.google.com/presentation/d/1MbltRcOerktc-E26HMDjYj0BO9CTubQWu1Z2bB9CpVY/edit#slide=id.g448ccc227721fe56_10
+
+#Simplified GitHub- GitHub Desktop
+https://help.github.com/en/desktop
+
+#Exercise
+#1. Create a test repository, call it "Junk"
+#2. Commit copies of a random R script and a random text .txt file
+#3. Pair up with a buddy. Follow them on GitHub
+#4. Fork their Junk repository
+#5. Report an Issue on their repository. See slides #47-53
+#6. Create a pull request on their repository. See slides #54-68
+#7. Resolve the issues and pull request for your own GitHub account.
+#8. Revel in your nerd powers. Watch Star Trek reruns. Eat Pringles.
+#
+
+#Hosting a web page: See 6 minute mark
+#https://www.youtube.com/watch?v=A07QI3_Ie34&feature=youtu.be
+#
+#username.github.io/repo name
+#
+#Best practices 
+#https://github.com/BuzzFeedNews/everything
+#
+#Exercise:
+#Andrew Ba Tran: This exercise is for power users, 
+#install.packages("usethis")
+#usethis::use_course("https://github.com/r-journalism/learn-chapter-7/archive/master.zip", destdir =  '/Users/rswells/Dropbox/Classes/Data-Analysis-Class-Jour-405v-5003/')
+2
+
+#Using this tutorial: http://learn.r-journalism.com/en/git/git/git/
+#alternatives: bitbucket gitlab
+#1. Create html
+
+
+
+
+#The whole process of configuing git is for your home computer
+#
+#Add Folder
+#Commit Folder
+#
+#See github.rmd, line 136
+
+
+#
+#Interactive exercise to learn git branching: https://learngitbranching.js.org/
+
+#
+#github for for the useR
+https://happygitwithr.com/index.html
